254593c91d162198520dc2eb661f280cb5f366e7
manifestori <ori@manifestcyber.com>
2023-10-31 16:59:04 +0100

    ci: test orb

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -6,15 +6,13 @@ name=$(circleci env subst "${MANIFEST_SOURCE_NAME}")
 version=$(circleci env subst "${MANIFEST_SOURCE_VERSION}")
 format=$(circleci env subst "${MANIFEST_FORMAT}")
 sources=$(circleci env subst "${MANIFEST_SOURCE}")
-ptargs=$(circleci env subst "${MANIFEST_ARGS}")
+args=$(circleci env subst "${MANIFEST_ARGS}")
 
 sources=${sources//,/}
 
-tptargs=$(echo "${ptargs}" | awk '{$1=$1};1')
-
-if [[ -z "${tptargs}" ]]; then
-    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}"
-else
-    echo "using ptargs: ${ptargs}"
-    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}" -- "${ptargs}"
+targs=$(echo "${args}" | awk '{$1=$1};1')
+if [[ -n ${targs} ]]; then
+    targs="-- ${targs}"
 fi
+
+manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}" "${targs}"
