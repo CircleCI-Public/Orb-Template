a5c3a3c9f96070d80f96e9bffea7de1e54aa3f95
manifestori <ori@manifestcyber.com>
2023-10-31 17:21:07 +0100

    ci: test orb

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -7,13 +7,14 @@ version=$(circleci env subst "${MANIFEST_SOURCE_VERSION}")
 format=$(circleci env subst "${MANIFEST_FORMAT}")
 sources=$(circleci env subst "${MANIFEST_SOURCE}")
 ptargs=$(circleci env subst "${MANIFEST_ARGS}")
+publish=$(circleci env subst "${MANIFEST_PUBLISH}")
 
 sources=${sources//,/}
 
 tptargs=$(echo "${ptargs}" | awk '{$1=$1};1')
 
 if [[ -z "${tptargs}" ]]; then
-    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}"
+    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" --publish="${publish}" "${sources}"
 else
-    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}" -- "${ptargs}"
+    manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
 fi
