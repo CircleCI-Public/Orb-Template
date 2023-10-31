64145a8bbce928dc38dd3fccb95bc6d26b5ace10
manifestori <ori@manifestcyber.com>
2023-10-31 14:57:47 +0100

    ci: test orb

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -5,8 +5,10 @@ generator=$(circleci env subst "${MANIFEST_GENERATOR}")
 name=$(circleci env subst "${MANIFEST_SOURCE_NAME}")
 version=$(circleci env subst "${MANIFEST_SOURCE_VERSION}")
 format=$(circleci env subst "${MANIFEST_FORMAT}")
-sources=$(circleci env subst "${MANIFEST_SOURCES}")
+sources=$(circleci env subst "${MANIFEST_SOURCE}")
 args=$(circleci env subst "${MANIFEST_ARGS}")
 
-sources=${sources//,/ }
+echo "sources: ${sources}"
+sources=${sources//,/}
+echo "sources: ${sources}"
 manifest sbom --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" "${sources}" -- "${args}"
