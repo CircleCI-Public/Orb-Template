1c9bf705da29c3bebe81391b9a9b9089745387bc
manifestori <ori@manifestcyber.com>
2023-10-31 14:49:58 +0100

    ci: test orb

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -8,8 +8,8 @@ generator_version=${generator_version:-"latest"}
 
 # only accept syft versions equal or lower than v0.92.0
 if [[ "${generator}" == "syft" ]]; then
-    gversion=$(echo "${generator_version}" | sed 's/v//g')
-    if [[ "${gversion}" > "v0.92.0" ]]; then
+    gversion=${generator_version#"v"}
+    if [[ "${gversion}" > "0.92.0" ]]; then
         echo "syft versions equal or lower than v0.92.0 are supported"
         exit 1
     fi
