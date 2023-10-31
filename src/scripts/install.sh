7d3dafedfac011c9c0303a4626c49f716730a290
manifestori <ori@manifestcyber.com>
2023-10-31 14:42:10 +0100

    ci: test orb

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,10 +4,11 @@ dir=$(circleci env subst "${MANIFEST_INSTALL_DIR}")
 version=$(circleci env subst "${MANIFEST_CLI_VERSION}")
 generator=$(circleci env subst "${MANIFEST_GENERATOR}")
 generator_version=$(circleci env subst "${MANIFEST_GENERATOR_VERSION}")
+generator_version=${generator_version:-"latest"}
 
 dir=${dir:-"/usr/local/bin"}
 curl -sSL https://raw.githubusercontent.com/manifest-cyber/cli/main/install.sh | sudo sh -s -- -b "${dir}" "${version}"
 
 if [[ -n "${generator}" ]]; then
-    manifest install -g "${generator}" -v "${generator_version}"
+    sudo manifest install -g "${generator}" -v "${generator_version}" || exit 1
 fi
