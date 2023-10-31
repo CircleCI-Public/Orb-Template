75515d621b05bd26cb550776cc4bff32bfac32dc
manifestori <ori@manifestcyber.com>
2023-10-31 10:29:04 +0100

    ci: test orb

--- src/scripts/install_manifest.sh
+++ src/scripts/install_manifest.sh
@@ -4,4 +4,4 @@ dir=$(circleci env subst "${INSTALL_DIR}")
 version=$(circleci env subst "${CLI_VERSION}")
 
 dir=${dir:-"/usr/local/bin"}
-curl -sSL https://raw.githubusercontent.com/manifest-cyber/cli/main/install.sh | sh -s -- -b "${dir}" "${version}"
+curl -sSL https://raw.githubusercontent.com/manifest-cyber/cli/main/install.sh | sudo sh -s -- -b "${dir}" "${version}"
