92a277708c17906d171ae9a3321292d6290b5058
manifestori <ori@manifestcyber.com>
2023-10-31 14:46:45 +0100

    ci: test orb

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -6,6 +6,14 @@ generator=$(circleci env subst "${MANIFEST_GENERATOR}")
 generator_version=$(circleci env subst "${MANIFEST_GENERATOR_VERSION}")
 generator_version=${generator_version:-"latest"}
 
+# only accept syft versions equal or lower than 0.92.0
+if [[ "${generator}" == "syft" ]]; then
+    if [[ "${generator_version}" > "0.92.0" ]]; then
+        echo "Syft version ${generator_version} is not supported. Please use version 0.92.0 or lower."
+        exit 1
+    fi
+fi
+
 dir=${dir:-"/usr/local/bin"}
 curl -sSL https://raw.githubusercontent.com/manifest-cyber/cli/main/install.sh | sudo sh -s -- -b "${dir}" "${version}"
 
